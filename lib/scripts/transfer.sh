05926bb5c08b82851d21af9c35b77cc4c2d1402f
aadame3311 <antonio.adame01@utrgv.edu>
2019-06-25 10:46:56 -0500

    changed password var to env var

--- lib/scripts/transfer.sh
+++ lib/scripts/transfer.sh
@@ -2,11 +2,11 @@
 
 echo going to transfer $1 to the remote server and call it $2
 
-sshpass -p 'PASSWORD' scp -P 2222  $1 root@sandbox-hdp.hortonworks.com:~/$2
+sshpass -p $SAMPLE_PASSWORD scp -P 2222  $1 root@sandbox-hdp.hortonworks.com:~/$2
 
 echo 'file was transfered, now going to add it hdfs'
 
-sshpass -p 'PASSWORD' ssh root@sandbox-hdp.hortonworks.com -p 2222 << EOF 
+sshpass -p $SAMPLE_PASSWORD ssh root@sandbox-hdp.hortonworks.com -p 2222 << EOF 
 
 echo 'making the folder structure in hdfs';
 
